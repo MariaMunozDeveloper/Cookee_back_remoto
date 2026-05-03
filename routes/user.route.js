66202284b87a7ab91b1824bd41c398ccb325a678
Mireia <munozferrer.maria@gmail.com>
2026-05-03 13:57:17 +0200

    acabando

--- routes/user.route.js
+++ routes/user.route.js
@@ -11,7 +11,7 @@ router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.put('/update', authMiddleware.ensureAuth, userController.updateUser);
 router.get('/', userController.listUsers);
-router.get('/user/:id', authMiddleware.ensureAuth, userController.getUserById);
+router.get('/:id', authMiddleware.ensureAuth, userController.getUserById);
 router.post('/upload-avatar', authMiddleware.ensureAuth, uploadMiddleware.single('avatar'), userController.uploadAvatar);
 router.get('/counters', authMiddleware.ensureAuth, userController.getCounters);
 router.get('/counters/:id', authMiddleware.ensureAuth, userController.getCounters);
